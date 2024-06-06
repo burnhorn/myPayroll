9e6ee1a7f03a63eb040bb68712cc2134b0f95a59
burnhorn <jgk20220202@gmail.com>
2024-06-06 19:59:48 +0900

    change domain.question_router

--- domain/Question/question_router.py
+++ domain/Question/question_router.py
@@ -5,20 +5,20 @@ from domain.Question import question_schema, question_crud
 from database import get_db
 
 router = APIRouter(
-    prefix = "/api"
+    prefix = "/api/question"
 )
 
 # 질문 여러 개체를 CRUD에서 가져오므로 응답모델을 list로 설정
-@router.get("/question/{quesiton_id}", response_model = question_schema.QuestionCreateResponse)
+@router.get("/create/{quesiton_id}", response_model = question_schema.QuestionCreateResponse)
 async def get_question(question_id : int, db:Session = Depends(get_db)):
     return question_crud.get_question(db, question_id = question_id)
 
-@router.post("/question", response_model = question_schema.QuestionCreateResponse)
+@router.post("/create", response_model = question_schema.QuestionCreateResponse)
 async def create_question(question_body : question_schema.QuestionCreate, db: Session = Depends(get_db)):
     return question_crud.create_question(db, question_body)
 
 
-@router.put("/question/{question_id}", response_model=question_schema.QuestionCreateResponse)
+@router.put("/update/{question_id}", response_model=question_schema.QuestionCreateResponse)
 async def update_question(question_id : int, question_body : question_schema.QuestionUpdate, db:Session = Depends(get_db)):
     question = question_crud.get_question(db, question_id = question_id)
     if question is None:
@@ -26,7 +26,7 @@ async def update_question(question_id : int, question_body : question_schema.Que
 
     return question_crud.update_question(db, question_body, question_original=question)
 
-@router.delete("/question/{question_id}", response_model = None)
+@router.delete("/delete/{question_id}", response_model = None)
 async def delete_question(question_id : int, db:Session = Depends(get_db)):
     question = question_crud.get_question(db, question_id = question_id)
     if question is None:
