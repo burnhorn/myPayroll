a74790b5d44f59d0181d05cda25435c28f78e5e2
burnhorn <jgk20220202@gmail.com>
2024-06-08 17:14:35 +0900

    modify pwd_context.hash

--- domain/User/user_crud.py
+++ domain/User/user_crud.py
@@ -11,7 +11,7 @@ def create_user(db:Session, user_create :user_schema.UserCreate) -> User:
     #user = User(**user_create.dict())
     user = User(user_name = user_create.user_name, password = user_create.password1, email = user_create.email)
     user = User(user_name = user_create.user_name,
-                password = pwd_context(user_create.password1),
+                password = pwd_context.hash(user_create.password1),
                 email = user_create.email)
     db.add(user)
     db.commit()
