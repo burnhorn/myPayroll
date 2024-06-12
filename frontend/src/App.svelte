d3bb2ed4eb2c2dbcbb16200308267fab5de5d0e4
burnhorn <jgk20220202@gmail.com>
2024-06-12 19:44:27 +0900

    setting frontend

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,1 +1,21 @@
-<h1>Hello test change</h1>
+<script>
+    async function getQuestionList() {
+        const res = await fetch("http://127.0.0.1:8000/api/insurance/rates");
+        const json = await res.json();
+
+        if (res.ok) {
+            return json.message;
+        }
+        else {
+            alert("error");
+        }
+    }
+
+    let promise = getQuestionList();
+</script>>
+
+{#await Promise}
+<p>Process Waiting</p>
+{:then message}
+<h1>{meassage}</h1>
+</await>
