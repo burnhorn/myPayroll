64c62fa519358d4ec93f847c57ffee618be7edd9
burnhorn <jgk20220202@gmail.com>
2024-06-24 11:59:33 +0900

    modify insurance page

--- frontend/src/routes/insurance/cal_page.svelte
+++ frontend/src/routes/insurance/cal_page.svelte
@@ -21,21 +21,86 @@
 
 </script>
 
-<form method="post" class="my-1" on:submit={loadInsuranceCal}>
-    <div class="mb-3">
-        <label for="salary">급여 입력</label>
+<form method="post" class="form-container" on:submit={loadInsuranceCal}>
+    <div class="form-group">
+        <label for="salary">월 급여</label>
         <input type="number" class="form-control" bind:value={salary} id="salary">
     </div>
-    <button class="btn" type="submit">보험료 계산</button>
+    <button class="btn" type="submit">계산하기</button>
 </form>
 
 
-<ul>
 {#if salary_cal}
-    <li>국민연금 : {salary_cal.national_pension}</li>
-    <li>건강보험료 : {salary_cal.health_insurance}</li>
-    <li>장기요양료 : {salary_cal.medical_insurance}</li>
-    <li>고용보험료 : {salary_cal.employment_insurance}</li>
+  <div class="results-container">
+    <ul>
+      <li><strong>국민연금:</strong> {salary_cal.national_pension} 원</li>
+      <li><strong>건강보험료 :</strong> {salary_cal.health_insurance} 원</li>
+      <li><strong>장기요양료:</strong> {salary_cal.medical_insurance} 원</li>
+      <li><strong>고용보험료:</strong> {salary_cal.employment_insurance} 원</li>
+    </ul>
+  </div>
 {/if}
-</ul>
-  
+
+<style>
+  .form-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center; /* 중앙 정렬 */
+  }
+
+  .form-group {
+    display: flex;
+    align-items: center; /* 수직 가운데 정렬 */
+    justify-content: center; /* 수평 가운데 정렬 */
+    width: 100%;
+    max-width: 500px; /* 최대 너비 설정 */
+    margin-bottom: 1rem;
+  }
+
+  .form-group label {
+    margin-right: 1rem; /* 레이블과 입력 필드 사이의 간격 */
+    white-space: nowrap; /* 텍스트 줄바꿈 방지 */
+  }
+
+  .form-control {
+    flex: 1; /* 입력 필드가 남은 공간을 차지하게 함 */
+    padding: 0.5rem;
+    font-size: 1rem;
+  }
+
+  .btn {
+    background-color: #999898;
+    color: white;
+    padding: 0.5rem 1rem;
+    border: none;
+    border-radius: 0.25rem;
+    cursor: pointer;
+    font-size: 1rem;
+  }
+  .btn:hover {
+    background-color: #000000;
+  }
+  .results-container {
+    margin-top: 2rem;
+    padding: 1rem;
+    border: 1px solid #ddd;
+    border-radius: 0.25rem;
+    background-color: #f9f9f9;
+    max-width: 300px; /* 크기 조절 */
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .results-container ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  .results-container ul li {
+    margin-bottom: 0.5rem;
+    border-bottom: 1px solid #ddd; /* 아래쪽 선 추가 */
+    font-size: 1.1rem;
+  }
+  .results-container ul li strong {
+    margin-right: 0.5rem;
+  }
+</style>
